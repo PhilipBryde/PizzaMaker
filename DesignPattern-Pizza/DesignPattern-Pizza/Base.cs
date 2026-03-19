d8ddbf93e3466ab15ee6baac13e2acc02847a586
Philip Bryde <brydephilip@gmail.com>
2026-03-19 12:11:56 +0100

    A lot of changes, Solving merge conflicts and added actual intercations in the game

---
+++ DesignPattern-Pizza/DesignPattern-Pizza/Base.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern_Pizza
+{
+    public class Base : IPizza
+    {
+        public string GetDescription()
+        {
+            return "";
+        }
+
+        public decimal GetPrice()
+        {
+            return 0;
+        }
+    }
+}
